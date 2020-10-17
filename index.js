cd34f68b036006315461d2c6cb92495d11ecfde2
AbrahamS <bramjulians@gmail.com>
2020-10-17 01:00:15 -0700

    Get static page

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
+const staticHost = "https://static-links-page.signalnerve.workers.dev"
+const init ={
+  headers : {
+    "content-type": "text/html;charset=UTF-8",
+  }
+}
+
 addEventListener('fetch', event => {
+  
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
+async function gatherResponse(response) {
+  const { headers } = response
+  const contentType = headers.get("content-type") || ""
+  if (contentType.includes("application/json")) {
+    return JSON.stringify(await response.json())
+  }
+  else if (contentType.includes("application/text")) {
+    return await response.text()
+  }
+  else if (contentType.includes("text/html")) {
+    return await response.text()
+  }
+  else {
+    return await response.text()
+  }
+}
+
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  
+  
+  const response = await fetch(staticHost, init)
+  const results = await gatherResponse(response)
+  return new Response(results, init);
 }
